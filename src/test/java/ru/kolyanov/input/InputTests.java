665a59332091d990ac555f9fdc10b15be65edf1a
Andrey <andreykolyanov@github.com>
2016-05-12 01:53:10 +0600

    added tests

--- src/test/java/ru/kolyanov/input/InputTests.java
+++ src/test/java/ru/kolyanov/input/InputTests.java
@@ -3,30 +3,47 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class InputTests {
 
     IReader stringReader;
     @Test(expected = ReaderException.class)
-    public void testInput() throws ReaderException {
+    public void testInput() throws ReaderException, IOException {
         stringReader  = new StringReader("qw");
+        stringReader.close();
         assertEquals("input failed", stringReader.getSymbol(), 'q');
         assertEquals("input failed", stringReader.getSymbol(), 'w');
         assertEquals("input failed", stringReader.getSymbol(), 'e');
     }
 
     @Test(expected = ReaderException.class)
-    public void testInput2() throws ReaderException {
+    public void testInput2() throws ReaderException, IOException {
         stringReader  = new StringReader(null);
+        stringReader.close();
         assertEquals("input failed", stringReader.getSymbol(), 'q');
         assertEquals("input failed", stringReader.getSymbol(), 'w');
         assertEquals("input failed", stringReader.getSymbol(), 'e');
     }
 
     @Test(expected = ReaderException.class)
-    public void testInput3() throws ReaderException {
+    public void testInput3() throws ReaderException, IOException {
         stringReader  = new FileReader("//");
+        stringReader.close();
         assertEquals("input failed", stringReader.getSymbol(), 'q');
         assertEquals("input failed", stringReader.getSymbol(), 'w');
         assertEquals("input failed", stringReader.getSymbol(), 'e');
+    }
+
+    @Test(expected = ReaderException.class)
+    public void testInput4() throws ReaderException, IOException {
+        File tmp = File.createTempFile("data", null);
+        stringReader  = new FileReader(tmp.getAbsolutePath());
+        stringReader.close();
+        assertEquals("input failed", stringReader.getSymbol(), 'q');
+        assertEquals("input failed", stringReader.getSymbol(), 'w');
+        assertEquals("input failed", stringReader.getSymbol(), 'e');
+
     }
 }
--- src/test/java/ru/kolyanov/output/OutputTests.java
+++ src/test/java/ru/kolyanov/output/OutputTests.java
@@ -3,14 +3,37 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 public class OutputTests {
 
-    StringWriter writer;
+    IWriter writer;
     @Test
     public void testOutput() throws WritingException{
         writer = new StringWriter();
         writer.printLine("hello");
         writer.printLine("world");
+        writer.write();
+        assertEquals("output failed", "hello\nworld\n", writer.getLines());
+    }
+
+    @Test
+    public void testOutput2() throws WritingException, IOException {
+        File tmp = File.createTempFile("out", null);
+        writer = new FileWriter(tmp.getAbsolutePath());
+        writer.printLine("hello");
+        writer.printLine("world");
+        assertEquals("output failed", "hello\nworld\n", writer.getLines());
+    }
+
+    @Test
+    public void testOutput3() throws WritingException, IOException {
+        File tmp = File.createTempFile("out", null);
+        writer = new FileWriter("//");
+        writer.printLine("hello");
+        writer.printLine("world");
+        writer.write();
         assertEquals("output failed", "hello\nworld\n", writer.getLines());
     }
 }
